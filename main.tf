19fb463c2352223b84028c662a4dca740888574a
igortyb <84392379+igortyb@users.noreply.github.com>
2021-05-20 16:59:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,20 @@ variable "instance_type" {
   default = "t3.micro"
   type    = string
 }
-
+variable "subnet" {
+  default = ""
+  type    = string
+}
+variable "name" {
+  default = ""
+  type    = string
+}
+data "aws_subnet" "subnet" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.subnet}"]
+  }
+}
 data "aws_ami" "ubuntu" {
   most_recent = true
   filter {
@@ -15,23 +28,11 @@ data "aws_ami" "ubuntu" {
   }
   owners = ["099720109477"] # Canonical
 }
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-  
+  subnet_id     = data.aws_subnet.subnet.id
   tags = {
-    Name = "HelloWorld"
+    Name = "${var.name}"
   }
-}
-
-module "vm1" {
-  source = "github.com/igortyb/kurs-actions.git"
-  subnet = "private-a"
-  name   = "vm1"
-}
-module "vm2" {
-  source = "github.com/igortyb/kurs-actions.git"
-  subnet = "private-b"
-  name   = "vm2"
 }
